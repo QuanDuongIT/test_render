2acce5b14fda548a8906683a29826b6c5154dc61
Duong <tieuduong.25.1.98@gmail.com>
2026-05-13 09:25:58 +0700

    first push

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # test_render
+
+# venv\Scripts\activate
+# python -m ensurepip --upgrade
+# pip install flask gunicorn
+# pip install -r requirements.txt
+# python -m pip install flask gunicorn
---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Hello Render!</h1>
+    <p>Website Python Flask đầu tiên của tôi.</p>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
